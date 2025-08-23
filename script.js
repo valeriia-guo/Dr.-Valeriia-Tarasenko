e2dbbe8a772891e786b0b2a2c0ed40bf9a739f96
valeriia-guo <valerat1227@gmail.com>
2025-08-23 15:14:23 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,12 @@ window.addEventListener('scroll', () => {
 });
 
 function readMore() {
-    var expandInfo = document.getElementById("more-info-js");
-    var mainHeadings = document.getElementById("main-headings-js");
-    
-    mainHeadings.style.transform = "scale(0.7)";
-    expandInfo.style.height = "350px";
-  } 
-
+  var expandInfo = document.getElementById("more-info-js");
+  var mainHeadings = document.getElementById("main-headings-js");
+  
+  mainHeadings.style.transform = "scale(0.7)";
+  expandInfo.style.height = "350px";
+} 
 
 
 
